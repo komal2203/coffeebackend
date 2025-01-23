4748dc98936c3f9090fd7c8210edaecaa980283f
Komal Meena <capkomal2203@gmail.com>
2025-01-23 20:55:14 +0530

    done

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get("/githubDataa", (req, res) => {
   res.send(githubData)
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   console.log(`happy happy`);
 });
